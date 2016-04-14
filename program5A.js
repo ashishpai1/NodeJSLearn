7f183772bec32d3ee878aa4c8f15b49a778b0852
Ashish Pai <ashish.pai.git@gmail.com>
2016-04-13 22:24:54 -0700

    BufferList

---
+++ program5A.js
@@ -0,0 +1,16 @@
+var bl = require('bl');
+var http = require('http');
+
+http.get(process.argv[2], function(response){
+  	response.setEncoding("utf8");
+	response.pipe(bl(function (err, data) {
+		 if (err)  
+           return console.error(err)  
+         data = data.toString()  
+         console.log(data.length)  
+         console.log(data);  
+	})); 
+})
+  
+   
+  
